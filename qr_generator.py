41000b352e69528245ffc948f9e552ff0917b33e
RICARDO ZUÑIGA GALARZA <rizagal@hotmail.com>
2024-08-07 04:38:26 -0500

    Update qr_generator.py

--- qr_generator.py
+++ qr_generator.py
@@ -17,12 +17,12 @@ def generate_qr_code(url, filename):
     img.save(filename)
 
 #create a streamlit app 
-st.set_page_config(page_title="QR Code Generator", page_icon="🌐", layout="centered")
+st.set_page_config(page_title="Generador Codigo QR", page_icon="🌐", layout="centered")
 st.image("images/supports.JPG", use_column_width=True)
 st.title("Generador de Codigo Qr")
 url = st.text_input("Ingrese su URL")
 
-if st.button("Generar QR Code"):
+if st.button("Generar Codigo QR"):
     generate_qr_code(url, filename)
     st.image(filename, use_column_width=True)
     with open(filename, "rb") as f:
