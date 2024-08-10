e669412b48f03324c1915805d9d373ecb38e573d
RICARDO ZUÑIGA GALARZA <rizagal@hotmail.com>
2024-08-10 09:03:48 -0500

    Update qr_generator.py

--- qr_generator.py
+++ qr_generator.py
@@ -26,8 +26,14 @@ st.sidebar.image("ideabien.PNG",caption="")
 with st.sidebar:
 
     st.warning("debes estar subscrito")
-    add_auth(required=True)
+    add_auth(
+    required=True,
+    login_button_text="Iniciar con Google",
+    login_button_color="#FD504D",
+    login_sidebar=True,
+    ) 
     st.success("Bienvenido")
+    st.write(str(st.session_state.email))
     selected = option_menu('Aplicativos Web con Sus respectivas explicacion y Codigo',
                            ['Codigo QR en Paython',
                             'Consulta Resultado Indicadores Atencion al Usuario', 
