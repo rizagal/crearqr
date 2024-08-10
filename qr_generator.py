a1937faed284d5de74cb3cfe78bdda5adc5c7c4c
RICARDO ZUÑIGA GALARZA <rizagal@hotmail.com>
2024-08-10 05:47:17 -0500

    Update qr_generator.py

--- qr_generator.py
+++ qr_generator.py
@@ -1,7 +1,11 @@
 import qrcode
 import streamlit as st
+from streamlit_option_menu import option_menu
+from st_paywall import add_auth
 
 filename = "qr_codes/qr_code.png"
+
+st.set_page_config(page_title="QR Code Generator", page_icon="🌐", layout="centered")
 
 def generate_qr_code(url, filename):
     qr = qrcode.QRCode(
@@ -16,18 +20,31 @@ def generate_qr_code(url, filename):
     img = qr.make_image(fill_color="black", back_color="white")
     img.save(filename)
 
-#create a streamlit app 
-st.set_page_config(page_title="Generador Codigo QR", page_icon="🌐", layout="centered")
-st.image("images/supports.JPG", use_column_width=True)
-st.title("Generador de Codigo QR")
-url = st.text_input("Ingrese su URL")
 
-if st.button("Generar Codigo QR"):
-    generate_qr_code(url, filename)
-    st.image(filename, use_column_width=True)
-    with open(filename, "rb") as f:
-                    image_data = f.read()  
-    download = st.download_button(label="Download QR", data=image_data, file_name="qr_generated.png")
+st.sidebar.image("SIGES17.png",caption="")
+
+with st.sidebar:
+
+    st.warning("debes estar subscrito")
+    add_auth(required=True)
+    st.success("Bienvenido")
+    selected = option_menu('Aplicativos Web con Sus respectivas explicacion y Codigo',
+                           ['Codigo QR en Paython',
+                            'Consulta Resultado Indicadores Atencion al Usuario', 
+                            ],
+                           icons = ['activity','bar-chart'],
+                           default_index = 0)
 
 
+if(selected == 'Codigo QR en Paython'):
+#create a streamlit app     
+    st.image("images/supports.JPG", use_column_width=True)
+    st.title("QR Code Generator")
+    url = st.text_input("Enter the URL")
 
+    if st.button("Generate QR Code"):
+        generate_qr_code(url, filename)
+        st.image(filename, use_column_width=True)
+        with open(filename, "rb") as f:
+                        image_data = f.read()  
+        download = st.download_button(label="Download QR", data=image_data, file_name="qr_generated.png")
