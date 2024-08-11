bbb979bc941260a34c4b6eb429c097d3ef105947
RICARDO ZUÑIGA GALARZA <rizagal@hotmail.com>
2024-08-11 12:40:28 -0500

    Update qr_generator.py

--- qr_generator.py
+++ qr_generator.py
@@ -24,7 +24,7 @@ def generate_qr_code(url, filename):
 st.sidebar.image("ideabien.PNG",caption="")
 
 with st.sidebar:  
-    if 'key' not in st.session_state.user_subscribed:
+    if (session_state.user_subscribed == True):
         st.warning("debes estar subscrito") 
     add_auth(
     required=True,
